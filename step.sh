56f5563e78da3aa20f914a9bd226cfcd06db3d82
Spiros Gerokostas <s.gerokostas@kaizengaming.com>
2023-09-25 16:28:46 +0300

    Update step values

--- step.sh
+++ step.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-is_mr_draft=$(curl --request GET --header "PRIVATE-TOKEN: ${gitlab_user_token}" -H "Accept: application/json" "${gitlab_api_url}/projects/${project_id}/merge_requests/${bitrise_pull_request}" | ruby -rjson -e 'puts JSON.parse($stdin.read)["draft"]')
-
-echo $is_mr_draft
+is_mr_draft=$(curl --request GET --header "PRIVATE-TOKEN: ${gitlab_bot_token}" -H "Accept: application/json" "${gitlab_api_url}/projects/${project_id}/merge_requests/${bitrise_pull_request}" | ruby -rjson -e 'puts JSON.parse($stdin.read)["draft"]')
 
 envman add --key IS_MR_DRAFT --value $is_mr_draft
