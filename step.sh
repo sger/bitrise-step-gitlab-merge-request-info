bd6d3e96c9e394751e7b2c185b6568ddefbc07ba
Spiros Gerokostas <s.gerokostas@kaizengaming.com>
2023-09-25 16:21:52 +0300

    Add input and output values

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-is_mr_draft=$(curl --request GET --header "PRIVATE-TOKEN: ${gitlab_bot_token}" -H "Accept: application/json" "${gitlab_api_url}/projects/${project_id}/merge_requests/${bitrise_pull_request}" | ruby -rjson -e 'puts JSON.parse($stdin.read)["draft"]')
+is_mr_draft=$(curl --request GET --header "PRIVATE-TOKEN: ${gitlab_user_token}" -H "Accept: application/json" "${gitlab_api_url}/projects/${project_id}/merge_requests/${bitrise_pull_request}" | ruby -rjson -e 'puts JSON.parse($stdin.read)["draft"]')
 
 echo $is_mr_draft
 
